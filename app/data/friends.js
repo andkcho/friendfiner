02f7d5c802343444d0c0b2626ef41a9c70796a80
andkcho <{andkcho}@users.noreply.github.com>
2018-03-12 20:14:12 -0600

    update

---
+++ app/data/friends.js
@@ -0,0 +1,90 @@
+// Data structure that will store the friends data
+var friends = [
+  {
+      name: "Matt",
+      photo: "https://photos.app.goo.gl/aIwMILVj13ZxPHNQ2",
+      scores: [
+      "3",
+      "4",
+      "4",
+      "3",
+      "4",
+      "2",
+      "4",
+      "2",
+      "4",
+      "3"
+      ]
+  },
+
+  {
+      name: "Amy",
+      photo: "https://photos.app.goo.gl/i1inwdsFWDZMezCj1",
+      scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "3",
+      "2",
+      "2",
+      "4",
+      "4",
+      "2"
+      ]
+  },
+
+  {
+      name: "Olivia",
+      photo: "https://photos.app.goo.gl/gcpNpg5wH9L6c8V22",
+      scores: [
+      "5",
+      "2",
+      "5",
+      "2",
+      "3",
+      "2",
+      "3",
+      "1",
+      "2",
+      "5"
+      ]
+  },
+
+  {
+      name: "Steve",
+      photo: "https://photos.app.goo.gl/k3CMyfSrl0q9WMex2",
+      scores: [
+      "4",
+      "4",
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "2",
+      "5"
+      ]
+  },
+
+  {
+      name: "Aaron",
+      photo: "https://clutchpoints.com/wp-content/uploads/2017/12/Aaron-Rodgers-2.png",
+      scores: [
+      "4",
+      "2",
+      "1",
+      "5",
+      "3",
+      "2",
+      "5",
+      "2",
+      "2",
+      "5"
+      ]
+  }
+
+];
+
+module.exports = friends;
